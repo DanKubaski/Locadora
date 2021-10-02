51986d45684b1a3e183ba43298e90ece6c98d82c
Danilo Kubaski <danilokubaski@gmail.com>
2021-10-02 19:28:36 -0300

    atualizacao

--- app/Models/Modelo.php
+++ app/Models/Modelo.php
@@ -14,7 +14,7 @@ class UserModel extends Model
     protected $returnType     = 'array';
     protected $useSoftDeletes = true;
 
-    protected $allowedFields = ['name', 'email'];
+    protected $allowedFields = ['TB_CLIENTE_NOME', 'TB_CLIENTE_TEL', '	TB_CLIENTE_SEXO', 'TB_CLIENTE_EMAIL', '	TB_CLIENTE_SENHA', 'TB_CLIENTE_ENDERECO', '	TB_CLIENTE_COMPLEMENTO', '	TB_CLIENTE_BAIRRO', '	TB_CLIENTE_CIDADE', 'TB_CLIENTE_UF', 'TB_CLIENTE_DT_NASC', 'now()'];
 
     protected $useTimestamps = false;
     protected $createdField  = 'created_at';
